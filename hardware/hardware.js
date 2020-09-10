7c6ef643d4ec3aac440ef92aba3beeb420529439
markdhooper <markushooper@gmail.com>
2020-09-10 16:27:32 -0300

    created a hardware page with tabbed content. function calls are set up for running python scripts in hardware/helper_scripts to obtain/parse system information

--- hardware/hardware.js
+++ hardware/hardware.js
@@ -1,20 +1,40 @@
-function test()
+function motherboard()
 {
-	var test = document.getElementById("test_output");
-	test.innerhtml = "BUTTON WAS CLICKED";
+	var test = document.getElementById("motherboard_output");
+	test.innerHTML = "Motherboard";
 }
 
-function reset_test()
+function system()
 {
-	var test = document.getElementById("test_output");
-	test.innerhtml = " ";
+	var test = document.getElementById("system_output");
+	test.innerHTML = "System";
 }
 
+function rear()
+{
+	var test = document.getElementById("rear_output");
+	test.innerHTML = "Rear";
+}
+
+function disks()
+{
+	var test = document.getElementById("disks_output");
+	test.innerHTML = "Disks";
+}
+
+function power()
+{
+	var test = document.getElementById("power_output");
+	test.innerHTML = "Power";
+}
 
 function main()
 {
-	document.getElementById("clickme").addEventListener("click", test);
-	document.getElementById("reset").addEventListener("click", reset_test);
+	document.getElementById("system_tab_link").addEventListener("click", system);
+	document.getElementById("motherboard_tab_link").addEventListener("click", motherboard);
+	document.getElementById("rear_tab_link").addEventListener("click", rear);
+	document.getElementById("disks_tab_link").addEventListener("click", disks);
+	document.getElementById("power_tab_link").addEventListener("click", power);
 }
 
 main();
