bb91ee6d3c8f4368e2f1ae225162b6441236848b
markdhooper <markushooper@gmail.com>
2020-09-11 16:31:19 -0300

    added additional features to motherboard python script, able to obtain Motherboard, CPU, Sensor Readings, System (45drives) information, and HBA card information

--- usr/share/cockpit/hardware/hardware.js
+++ usr/share/cockpit/hardware/hardware.js
@@ -1,12 +1,13 @@
+
+//listener for clicking on the motherboard tab
 function motherboard()
 {
 	var dfd = cockpit.defer();
 	var m_output = document.getElementById("motherboard_output");
-	//m_output.innerHTML = "Motherboard";
 	var proc = cockpit.spawn(
 			[
 				"/usr/bin/pkexec",
-				"/usr/share/cockpit/hardware/helper_scripts/python_stdout_test"
+				"/usr/share/cockpit/hardware/helper_scripts/motherboard"
 			], 
 			{err: "out"}
 	);
@@ -20,24 +21,28 @@ function motherboard()
 	);
 }
 
+//listener for clicking on the system tab
 function system()
 {
 	var test = document.getElementById("system_output");
 	test.innerHTML = "System";
 }
 
+//listener for clicking on the rear tab
 function rear()
 {
 	var test = document.getElementById("rear_output");
 	test.innerHTML = "Rear";
 }
 
+//listener for clicking on the disks tab
 function disks()
 {
 	var test = document.getElementById("disks_output");
 	test.innerHTML = "Disks";
 }
 
+//listener for clicking on the power tab
 function power()
 {
 	var test = document.getElementById("power_output");
