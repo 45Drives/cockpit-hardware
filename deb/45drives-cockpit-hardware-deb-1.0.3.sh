9cf28b6b03133e59ac600eb692d702643951f83f
markdhooper <markushooper@gmail.com>
2021-01-27 10:37:34 -0400

    modified build script for .deb

--- deb/45drives-cockpit-hardware-deb-1.0.3.sh
+++ deb/45drives-cockpit-hardware-deb-1.0.3.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 mkdir 45drives-cockpit-hardware_1.0.3-1
-git clone --branch 1.7.5 https://github.com/45Drives/cockpit-hardware.git
+git clone --branch 1.0.3 https://github.com/45Drives/cockpit-hardware.git
 cp -R cockpit-hardware/src/fakeroot/* 45drives-cockpit-hardware_1.0.3-1
 cp -r cockpit-hardware/deb/DEBIAN 45drives-cockpit-hardware_1.0.3-1/DEBIAN
 rm -rf cockpit-hardware
