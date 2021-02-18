778770c698ab291a08bd8a009f04fc6213f35bbe
markdhooper <markushooper@gmail.com>
2021-02-18 16:28:06 -0400

    fixed typo on changelog

--- deb/45drives-cockpit-hardware-deb-1.2.0.sh
+++ deb/45drives-cockpit-hardware-deb-1.2.0.sh
@@ -2,6 +2,6 @@
 mkdir 45drives-cockpit-hardware_1.2.0-2
 git clone --branch 1.2.0 https://github.com/45Drives/cockpit-hardware.git
 cp -R cockpit-hardware/src/fakeroot/* 45drives-cockpit-hardware_1.2.0-2
-cp -r cockpit-hardware/deb/DEBIAN/1.2.0-1 45drives-cockpit-hardware_1.2.0-2/DEBIAN
+cp -r cockpit-hardware/deb/DEBIAN/1.2.0-2 45drives-cockpit-hardware_1.2.0-2/DEBIAN
 rm -rf cockpit-hardware
 dpkg-deb --build 45drives-cockpit-hardware_1.2.0-2
