7d4da0a10d7e1f5b3ef79bab538127711d6ae334
bkelly16 <bkelly@45drives.com>
2021-02-23 11:50:49 -0400

    Update 45drives-cockpit-hardware-rpm-1.2.0.sh

--- rpm/45drives-cockpit-hardware-rpm-1.2.0.sh
+++ rpm/45drives-cockpit-hardware-rpm-1.2.0.sh
@@ -13,17 +13,17 @@ esac
 mkdir 45drives-temp
 cd 45drives-temp
 mkdir rpmbuild rpmbuild/RPMS rpmbuild/SOURCES rpmbuild/SPECS rpmbuild/SRPMS
-git clone --branch 1.2.0 https://github.com/45Drives/cockpit-hardware.git
-mkdir 45drives-cockpit-hardware-1.2.0
-cp -r cockpit-hardware/src/fakeroot/usr 45drives-cockpit-hardware-1.2.0/usr
-tar -zcvf 45drives-cockpit-hardware-1.2.0.tar.gz 45drives-cockpit-hardware-1.2.0/
-rm -rf 45drives-cockpit-hardware-1.2.0
-mv 45drives-cockpit-hardware-1.2.0.tar.gz rpmbuild/SOURCES/45drives-cockpit-hardware-1.2.0.tar.gz
-mv cockpit-hardware/rpm/45drives-cockpit-hardware-1.2.0.spec rpmbuild/SPECS/45drives-cockpit-hardware-1.2.0.spec
+git clone --branch master https://github.com/45Drives/cockpit-hardware.git
+mkdir cockpit-45drives-hardware-1.2.1
+cp -r cockpit-hardware/src/fakeroot/usr cockpit-45drives-hardware-1.2.1/usr
+tar -zcvf cockpit-45drives-hardware-1.2.1.tar.gz cockpit-45drives-hardware-1.2.1/
+rm -rf cockpit-45drives-hardware-1.2.1
+mv cockpit-45drives-hardware-1.2.1.tar.gz rpmbuild/SOURCES/cockpit-45drives-hardware-1.2.1.tar.gz
+mv cockpit-hardware/rpm/cockpit-45drives-hardware-1.2.1.spec rpmbuild/SPECS/cockpit-45drives-hardware-1.2.1.spec
 rm -rf cockpit-hardware
 rm -rf ~/rpmbuild
 cd ..
 cp -r 45drives-temp/rpmbuild ~/rpmbuild
 rm -rf 45drives-temp
 cd ~/rpmbuild
-rpmbuild -ba SPECS/45drives-cockpit-hardware-1.2.0.spec
+rpmbuild -ba SPECS/cockpit-45drives-hardware-1.2.1.spec
